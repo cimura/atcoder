ec632c4acf5d40fbd4389bc8576016fd4b22ded8
shunsuke <githubhoge@gmail.com>
2026-04-01 20:56:50 +0900

    Bの実装

--- ABC451/B-Personnel Change.cpp
+++ ABC451/B-Personnel Change.cpp
@@ -8,16 +8,25 @@ int main() {
   uint32_t n = 0, m = 0;
   cin >> n >> m;
 
-  vector<uint32_t> a[n], b[n];
+  //cout << "n: " << n << endl;
+  vector<uint32_t> a, b;
   for (int i = 0; i < n; ++i) {
     uint32_t A, B;
     cin >> A >> B;
-    a[i] = A;
-    b[i] = B;
+    a.push_back(A);
+    //cout << "a -> " << A << endl;
+    b.push_back(B);
+    //cout << "b -> " << B << endl;
   }
 
-  for (int j = 0; j < m; ++j) {
+  //cout << "m: " << m << endl;
+  for (uint32_t j = 1; j <= m; ++j) {
+    auto a_count = count(a.begin(), a.end(), j);
+    //cout << "a "<< a_count << endl;
+    auto b_count = count(b.begin(), b.end(), j);
+    //cout << "b "<< b_count << endl;
 
+    cout << b_count - a_count << endl;
   }
   return 0;
 }
