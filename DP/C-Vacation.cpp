58d4691d80bb2b26f8528037ef9c04f8bf8fc89c
cimy <githubhoge@gmail.com>
2026-04-05 23:06:50 +0900

    2次元のdp実装

--- DP/C-Vacation.cpp
+++ DP/C-Vacation.cpp
@@ -11,21 +11,19 @@ int max3(int a, int b, int c) {
 
 int main() {
   int n; cin >> n;
-  vector<long long> h(n);
+  vector<vector<long long>> dp(n, vector<long long>(3, 0));
 
-  if (n == 1) {
-    int a, b, c; cin >> a >> b >> c;
-    cout << max3 << endl; return 0;
-  }
+  int a, b, c;
+  cin >> a >> b >> c;
+  dp[0][0] = a;
+  dp[0][1] = b;
+  dp[0][2] = c;
 
-  for (int i = 0; i < n; ++i) {
-    int a, b, c; cin >> a >> b >> c;
-    int selected = 0;
-    // 前回maxで選んだやつは選びたくない
-    if (i > 0) {
-      selected = max3(a, b, c);
-      h[i] = max(h[i], h[i - 1] + selected);
-    }
+  for (int i = 1; i < n; ++i) {
+    cin >> a >> b >> c;
+    dp[i][0] = max(dp[i - 1][1] + a, dp[i - 1][2] + a);
+    dp[i][1] = max(dp[i - 1][0] + b, dp[i - 1][2] + b);
+    dp[i][2] = max(dp[i - 1][0] + c, dp[i - 1][1] + c);
   }
-  cout << h[n - 1] << endl;
+  cout << max3(dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]) << endl;
 }
