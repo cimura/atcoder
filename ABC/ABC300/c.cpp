255cfb4595a6de1d5f303590f814e735ca61128e
cimy <githubhoge@gmail.com>
2026-05-07 23:29:08 +0900

    精進

---
+++ ABC/ABC300/c.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define INF 1000000000
+
+using ll = long long;
+
+int main() {
+  int n; cin >> n;
+}
